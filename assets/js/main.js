409b3503f6743526181cb2100dcb5dc79c0deba4
Naseela-KT <75978405+Naseela-KT@users.noreply.github.com>
2023-07-06 23:07:36 +0530

    first-commit

---
+++ assets/js/main.js
@@ -0,0 +1,404 @@
+/**
+ * Template Name: Lonely
+ * Updated: May 30 2023 with Bootstrap v5.3.0
+ * Template URL: https://bootstrapmade.com/free-html-bootstrap-template-lonely/
+ * Author: BootstrapMade.com
+ * License: https://bootstrapmade.com/license/
+ */
+(function () {
+  "use strict";
+
+  /**
+   * Easy selector helper function
+   */
+  const select = (el, all = false) => {
+    el = el.trim();
+    if (all) {
+      return [...document.querySelectorAll(el)];
+    } else {
+      return document.querySelector(el);
+    }
+  };
+
+  /**
+   * Easy event listener function
+   */
+  const on = (type, el, listener, all = false) => {
+    let selectEl = select(el, all);
+    if (selectEl) {
+      if (all) {
+        selectEl.forEach((e) => e.addEventListener(type, listener));
+      } else {
+        selectEl.addEventListener(type, listener);
+      }
+    }
+  };
+
+  /**
+   * Easy on scroll event listener
+   */
+  const onscroll = (el, listener) => {
+    el.addEventListener("scroll", listener);
+  };
+
+  /**
+   * Navbar links active state on scroll
+   */
+  let navbarlinks = select("#navbar .scrollto", true);
+  const navbarlinksActive = () => {
+    let position = window.scrollY + 200;
+    navbarlinks.forEach((navbarlink) => {
+      if (!navbarlink.hash) return;
+      let section = select(navbarlink.hash);
+      if (!section) return;
+      if (
+        position >= section.offsetTop &&
+        position <= section.offsetTop + section.offsetHeight
+      ) {
+        navbarlink.classList.add("active");
+      } else {
+        navbarlink.classList.remove("active");
+      }
+    });
+  };
+  window.addEventListener("load", navbarlinksActive);
+  onscroll(document, navbarlinksActive);
+
+  /**
+   * Scrolls to an element with header offset
+   */
+  const scrollto = (el) => {
+    let header = select("#header");
+    let offset = header.offsetHeight;
+
+    if (!header.classList.contains("header-scrolled")) {
+      offset -= 16;
+    }
+
+    let elementPos = select(el).offsetTop;
+    window.scrollTo({
+      top: elementPos - offset,
+      behavior: "smooth",
+    });
+  };
+
+  /**
+   * Header fixed top on scroll
+   */
+  let selectHeader = select("#header");
+  if (selectHeader) {
+    let headerOffset = selectHeader.offsetTop;
+    let nextElement = selectHeader.nextElementSibling;
+    const headerFixed = () => {
+      if (headerOffset - window.scrollY <= 0) {
+        selectHeader.classList.add("fixed-top");
+        nextElement.classList.add("scrolled-offset");
+      } else {
+        selectHeader.classList.remove("fixed-top");
+        nextElement.classList.remove("scrolled-offset");
+      }
+    };
+    window.addEventListener("load", headerFixed);
+    onscroll(document, headerFixed);
+  }
+
+  /**
+   * Back to top button
+   */
+  let backtotop = select(".back-to-top");
+  if (backtotop) {
+    const toggleBacktotop = () => {
+      if (window.scrollY > 100) {
+        backtotop.classList.add("active");
+      } else {
+        backtotop.classList.remove("active");
+      }
+    };
+    window.addEventListener("load", toggleBacktotop);
+    onscroll(document, toggleBacktotop);
+  }
+
+  /**
+   * Mobile nav toggle
+   */
+  on("click", ".mobile-nav-toggle", function (e) {
+    select("#navbar").classList.toggle("navbar-mobile");
+    this.classList.toggle("bi-list");
+    this.classList.toggle("bi-x");
+  });
+
+  /**
+   * Mobile nav dropdowns activate
+   */
+  on(
+    "click",
+    ".navbar .dropdown > a",
+    function (e) {
+      if (select("#navbar").classList.contains("navbar-mobile")) {
+        e.preventDefault();
+        this.nextElementSibling.classList.toggle("dropdown-active");
+      }
+    },
+    true
+  );
+
+  /**
+   * Scrool with ofset on links with a class name .scrollto
+   */
+  on(
+    "click",
+    ".scrollto",
+    function (e) {
+      if (select(this.hash)) {
+        e.preventDefault();
+
+        let navbar = select("#navbar");
+        if (navbar.classList.contains("navbar-mobile")) {
+          navbar.classList.remove("navbar-mobile");
+          let navbarToggle = select(".mobile-nav-toggle");
+          navbarToggle.classList.toggle("bi-list");
+          navbarToggle.classList.toggle("bi-x");
+        }
+        scrollto(this.hash);
+      }
+    },
+    true
+  );
+
+  /**
+   * Scroll with ofset on page load with hash links in the url
+   */
+  window.addEventListener("load", () => {
+    if (window.location.hash) {
+      if (select(window.location.hash)) {
+        scrollto(window.location.hash);
+      }
+    }
+  });
+
+  /**
+   * Porfolio isotope and filter
+   */
+  window.addEventListener("load", () => {
+    let portfolioContainer = select(".portfolio-container");
+    if (portfolioContainer) {
+      let portfolioIsotope = new Isotope(portfolioContainer, {
+        itemSelector: ".portfolio-item",
+        layoutMode: "fitRows",
+      });
+
+      let portfolioFilters = select("#portfolio-flters li", true);
+
+      on(
+        "click",
+        "#portfolio-flters li",
+        function (e) {
+          e.preventDefault();
+          portfolioFilters.forEach(function (el) {
+            el.classList.remove("filter-active");
+          });
+          this.classList.add("filter-active");
+
+          portfolioIsotope.arrange({
+            filter: this.getAttribute("data-filter"),
+          });
+        },
+        true
+      );
+    }
+  });
+
+  /**
+   * Initiate portfolio lightbox
+   */
+  const portfolioLightbox = GLightbox({
+    selector: ".portfolio-lightbox",
+  });
+
+  /**
+   * Portfolio details slider
+   */
+  new Swiper(".portfolio-details-slider", {
+    speed: 400,
+    loop: true,
+    autoplay: {
+      delay: 5000,
+      disableOnInteraction: false,
+    },
+    pagination: {
+      el: ".swiper-pagination",
+      type: "bullets",
+      clickable: true,
+    },
+  });
+
+  /**
+   * Skills animation
+   */
+  let skilsContent = select(".skills-content");
+  if (skilsContent) {
+    new Waypoint({
+      element: skilsContent,
+      offset: "80%",
+      handler: function (direction) {
+        let progress = select(".progress .progress-bar", true);
+        progress.forEach((el) => {
+          el.style.width = el.getAttribute("aria-valuenow") + "%";
+        });
+      },
+    });
+  }
+
+  /**
+   * Testimonials slider
+   */
+  new Swiper(".testimonials-slider", {
+    speed: 600,
+    loop: true,
+    autoplay: {
+      delay: 5000,
+      disableOnInteraction: false,
+    },
+    slidesPerView: "auto",
+    pagination: {
+      el: ".swiper-pagination",
+      type: "bullets",
+      clickable: true,
+    },
+    breakpoints: {
+      320: {
+        slidesPerView: 1,
+        spaceBetween: 40,
+      },
+
+      1200: {
+        slidesPerView: 3,
+      },
+    },
+  });
+
+  /**
+   * Initiate Pure Counter
+   */
+  new PureCounter();
+})();
+
+
+// FORM
+
+function validateForm() {
+  var name = document.getElementById("name").value;
+  var mobilenumber = document.getElementById("mobilenumber").value;
+  var email = document.getElementById("email").value;
+  var subject = document.getElementById("subject").value;
+  var message = document.getElementById("message").value;
+ 
+  if (name==null || name==""){
+    Swal.fire({
+      icon: 'error',
+      title: "Error", 
+      text: 'Name cannot be Blank!',  
+      confirmButtonText: "Close", 
+    });
+  return false;
+  }
+
+  if (/\d/.test(name)){
+    Swal.fire({
+      icon: 'error',
+      title: "Error", 
+      text: 'Name cannot contain numbers!',  
+      confirmButtonText: "Close", 
+    });
+    return false;
+  }
+
+  if (mobilenumber=="" || mobilenumber==null) {
+    Swal.fire({
+      icon: 'error',
+      title: "Error", 
+      text: 'Phone cannot be Blank!',  
+      confirmButtonText: "Close", 
+    });
+    return false;
+  }
+
+  if (isNaN(mobilenumber)){
+    Swal.fire({
+      icon: 'error',
+      title: "Error", 
+      text: 'Phone cannot contain letters!',  
+      confirmButtonText: "Close", 
+    });
+    return false;
+  }
+  if((mobilenumber.charAt(0)!=9) && (mobilenumber.charAt(0)!=8) && (mobilenumber.charAt(0)!=7))
+  {
+    Swal.fire({
+      icon: 'error',
+      title: "Error", 
+      text: 'Phone must start with 9 or 8 or 7',  
+      confirmButtonText: "Close", 
+    });
+    return false;
+  }
+
+  if (mobilenumber.length<10 || mobilenumber.length>10){
+    Swal.fire({
+      icon: 'error',
+      title: "Error", 
+      text: 'Phone must contain 10 digits..',  
+      confirmButtonText: "Close", 
+    });
+    return false;
+  }
+  if(email==""){
+    Swal.fire({
+      icon: 'error',
+      title: "Error", 
+      text: 'Email cannot be Blank!',  
+      confirmButtonText: "Close", 
+    });
+    return false;
+  }
+
+  var emailRegex = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+  if (!emailRegex.test(email)) {
+    Swal.fire({
+      icon: 'error',
+      title: "Error", 
+      text: 'Email must be a valid email address',  
+      confirmButtonText: "Close", 
+    });
+    return false;
+
+  }
+
+  if (subject == "") {
+    Swal.fire({
+      icon: 'error',
+      title: "Error", 
+      text: 'Subject must be filled out',  
+      confirmButtonText: "Close", 
+    });
+    return false;
+  }
+
+
+  if (message == "") {
+    Swal.fire({
+      icon: 'error',
+      title: "Error", 
+      text: 'message must be filled out',  
+      confirmButtonText: "Close", 
+    });
+    return false;
+  }
+  
+  return Swal.fire({
+    icon:'succcess',
+    title: "Success", 
+    html: "Submited Sucessfully",  
+    confirmButtonText: "Ok",
+  });
+}
